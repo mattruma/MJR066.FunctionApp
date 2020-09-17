01211afc7f7e69f22c712bfc0517238e927c9e51
mattruma <mattruma@gmail.com>
2020-09-17 11:53:42 -0400

    deploy

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -1,15 +1,27 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
-try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
+async function run() {
+    const token = core.getInput("token");
+
+    const octokit = new github.GitHub(token);
+    const context = github.context;
+
+    if (!context.payload.issue) {
+        throw new Error("This should not happen");
+    }
+
+    const issue = await octokit.issues.get({
+        ...context.repo,
+        issue_number: context.payload.issue.number,
+    });
+
+    core.setOutput(
+        "exists",
+        issue.data.labels
+            .some((label) => label.name === core.getInput("label"))
+            .toString()
+    );
 }
+
+run();
