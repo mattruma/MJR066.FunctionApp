b4a55872dc93af47a02bf09d25eb12a4ad38ca0d
mattruma <mattruma@gmail.com>
2020-09-18 11:23:59 -0400

    create-issue

--- .github/actions/create-issue/index.js
+++ .github/actions/create-issue/index.js
@@ -1,8 +1,9 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
-// console.log(github.context.payload);
+console.log(github.context.payload);
 
-// const changelog = fs.readFileSync('', {
-//     encoding: "UTF8"
-// });
+const changelog = fs.readFileSync('template.md', {
+    encoding: "UTF8"
+});
