83bc8e056f3522bf38c8ca015fa5821068fd3446
mattruma <mattruma@gmail.com>
2020-09-18 11:57:55 -0400

    create-issue

--- .github/actions/create-issue/index.js
+++ .github/actions/create-issue/index.js
@@ -4,3 +4,18 @@ const github = require('@actions/github');
 console.log(github.context.payload);
 
 const octokit = new github.GitHub(core.getInput('token'));
+
+const title = core.getInput('title');
+const body = core.getInput('body');
+const assignees = core.getInput('assignees');
+const labels = core.getInput('labels');
+
+const response = await octokit.issues.create({
+    ...github.context.repo,
+    title,
+    body,
+    assignees: assignees ? assignees.split(',') : undefined,
+    labels: assignees ? assignees.split(',') : undefined
+})
+
+core.setOutput('issue', JSON.stringify(response.data));
