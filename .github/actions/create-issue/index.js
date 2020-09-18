764517961e089eec065cdfd618944547639ea5c4
mattruma <mattruma@gmail.com>
2020-09-18 11:18:16 -0400

    create-issue

--- .github/actions/create-issue/index.js
+++ .github/actions/create-issue/index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-console.log(github.context.payload);
+// console.log(github.context.payload);
 
 // const changelog = fs.readFileSync('', {
 //     encoding: "UTF8"
