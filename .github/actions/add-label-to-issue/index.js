a83f23fa432da7f7e22f77f6b684ce88cf38ec80
mattruma <mattruma@gmail.com>
2020-09-19 08:35:24 -0400

    add release label

---
+++ .github/actions/add-label-to-issue/index.js
@@ -0,0 +1,17 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+console.log(github.context.payload.issue);
+
+const octokit = new github.GitHub(core.getInput('token'));
+
+const issue_number = core.getInput('issue_number');
+const labels = core.getInput('labels');
+
+const response = octokit.issues.addLabels({
+    ...github.context.repo,
+    issue_number,
+    labels: labels ? labels.split(',') : undefined
+})
+
+core.setOutput('issue', JSON.stringify(response.data));
